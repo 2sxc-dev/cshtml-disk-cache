819427e21b98888ae0d8857cfe1f4fded791023e
Tonći Vatavuk <tonci@sistemi.hr>
2025-10-30 15:27:58 +0100

    cache invalidation testing

---
+++ 06-Test-XPS5.ps1
@@ -0,0 +1,45 @@
+#Requires -Version 5.1
+$urls = @(
+  "https://2sxc-dnn.dnndev.me/cshtml-disk-cache/p1",
+  "https://2sxc-dnn.dnndev.me/cshtml-disk-cache/p2",
+  "https://2sxc-dnn.dnndev.me/cshtml-disk-cache/p3",
+  "https://2sxc-dnn.dnndev.me/cshtml-disk-cache/p4"
+  "https://2sxc-dnn.dnndev.me/cshtml-disk-cache/p5",
+  "https://2sxc-dnn.dnndev.me/cshtml-disk-cache/p6"
+)
+
+New-Item -ItemType Directory -Force -Path ".\results" | Out-Null
+
+# # Cold run
+# .\Measure-Requests-XPS5.ps1 `
+#   -Urls $urls `
+#   -TouchWebConfigFirst `
+#   -CleanFirst `
+#   -StopAppPoolForCleanFirst `
+#   -TouchWebConfigEachRun `
+#   -CleanEachRun `
+#   -StopAppPoolForCleanEachRun `
+#   -Repeat 1 `
+#   -SaveCsv -CsvPath ".\results\05-cold-XPS5.csv"
+
+# # Short pause
+# Start-Sleep -Seconds 2
+
+# # Disk runs
+# .\Measure-Requests-XPS5.ps1 `
+#   -Urls $urls `
+#   -TouchWebConfigFirst `
+#   -TouchWebConfigEachRun `
+#   -Repeat 9 `
+#   -SaveCsv -CsvPath ".\results\05-disk-XPS5.csv"
+
+  # Short pause
+Start-Sleep -Seconds 2
+
+# Warm runs
+.\Measure-Requests-XPS5.ps1 `
+  -Urls $urls `
+  -DelayMsBetweenRequests 1000 `
+  -DelayMsBetweenRuns 5000 `
+  -Repeat 99 `
+  -SaveCsv -CsvPath ".\results\06-memory-XPS5.csv"
